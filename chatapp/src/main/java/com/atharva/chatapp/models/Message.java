a39585aedf9a3ed81abd97252f64c5e82e59f152
atharvasnighot <121221994+atharvasnighot@users.noreply.github.com>
2023-11-08 15:11:24 +0530

    Add files via upload

---
+++ chatapp/src/main/java/com/atharva/chatapp/models/Message.java
@@ -0,0 +1,20 @@
+package com.atharva.chatapp.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String name;
+    private String content;
+
+
+}
